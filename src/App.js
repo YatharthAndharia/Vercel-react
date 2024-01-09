110a75b4163afbc3b41ab4ac5710ebfc02ee3bd3
yatharth <yatharth.a@simformsolutions.com>
2024-01-09 12:26:48 +0530

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import { createClient, fetchExchange, cacheExchange } from 'urql';
 
 function App() {
+  const APIURL =
+    'https://subgraph.satsuma-prod.com/sw9vuxiQey3c/lyra/arbitrum-mainnet/api';
+
+  const posQuery = `
+  query{
+    positions(first:8000 orderBy:closeTimestamp orderDirection:desc where:{state:2})
+    {
+      id
+      positionId
+      owner
+      isLong
+      isBaseCollateral
+      state
+      openTimestamp
+      closeTimestamp
+      trades(orderBy:timestamp){
+        id
+        trader
+        transactionHash
+        isBuy
+        isOpen
+        closePNL
+        collateralUpdate{
+          collateralPNL
+          collateralAmountChange
+        }
+      }
+      collateralUpdates{
+        transactionHash
+        collateralAmountChange
+        collateralPNL
+        
+      }
+      closePNL
+      collateralPNL
+    }
+}
+`;
+
+  const getSubgraphData = async () => {
+    try {
+      const client = createClient({
+        url: APIURL,
+        exchanges: [cacheExchange, fetchExchange],
+      });
+      const data = await client.query(posQuery).toPromise();
+      console.log(data.data);
+      return data.data;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <button onClick={getSubgraphData}>FetchSubgraph</button>
       </header>
     </div>
   );
